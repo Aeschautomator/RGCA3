3baffa6a624d20cdd0c7deb853ccec5826341662
Alex esch <eschmassage@gmail.com>
2018-11-20 23:06:51 -0500

    burger.js with a few early modified lines in config and controllers

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Texas Toast Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Aztec Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Cheese Burger', true);
